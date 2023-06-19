e02649618727aa22ad05622330b08a982233f4b3
shreyajmera99 <ajmerashrey83@gmail.com>
2023-06-20 00:08:15 +0530

    decimal numbers

--- script.js
+++ script.js
@@ -35,45 +35,69 @@ const gp = (n) =>{
 // }
 
 const getData = () => {
-    const input = document.getElementById('num');
-    const numofwords = Number(input.value);
-  
-    if (isNaN(numofwords) || numofwords <= 0) {
-      showPopup();
-      return;
-    }
-  
-    const para = document.createElement('p');
-    let data = '';
-  
-    for (let i = 0; i < numofwords; ++i) {
-      const generatednumber = (Math.random() * 15).toFixed(0);
-      data += gp(generatednumber);
-      data += ' ';
-    }
-  
-    para.innerText = data;
-    para.setAttribute('class', 'paras');
-  
-    const container = document.querySelector('.container');
-    const existingPara = document.querySelector('.paras');
-  
-    if (existingPara) {
-      container.removeChild(existingPara);
-    }
-  
-    container.appendChild(para);
-  };
-  
-  const showPopup = () => {
-    const popup = document.getElementById('popup');
-    popup.style.display = 'block';
-  };
-  
-  const hidePopup = () => {
-    const popup = document.getElementById('popup');
-    popup.style.display = 'none';
-  };
+  const input = document.getElementById('num');
+  const numofwords = Number(input.value);
+
+  if (isNaN(numofwords) || numofwords <= 0 || numofwords % 1 !== 0) {
+    
+    showPopup();
+    return;
+  }
+
+  const para = document.createElement('p');
+  let data = '';
+
+  for (let i = 0; i < numofwords; ++i) {
+    const generatednumber = (Math.random() * 15).toFixed(0);
+    data += gp(generatednumber);
+    data += ' ';
+  }
+
+  para.innerText = data;
+  para.setAttribute('class', 'paras');
+
+  const container = document.querySelector('.container');
+  const existingPara = document.querySelector('.paras');
+
+  if (existingPara) {
+    container.removeChild(existingPara);
+  }
+
+  container.appendChild(para);
+
+  const copyButton = document.getElementById('copyButton');
+  copyButton.style.display = 'block';
+};
+
+const showPopup = () => {
+  const popup = document.getElementById('popup');
+  popup.style.display = 'block';
+};
+
+const hidePopup = () => {
+  const popup = document.getElementById('popup');
+  popup.style.display = 'none';
+};
+
+// const copyToClipboard = () => {
+//   const paraText = document.querySelector('.paras').innerText;
+
+//   const tempInput = document.createElement('input');
+//   tempInput.setAttribute('value', paraText);
+//   document.body.appendChild(tempInput);
+//   tempInput.select();
+//   document.execCommand('copy');
+//   document.body.removeChild(tempInput);
+
+//   alert('Text copied to clipboard!');
+// };
+
+
+
+
+
+
+
   
 
 
